c97b55ea51b9042f50867b36517504728d653acf
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-08 14:30:42 +0530

    feat: Create serializers and ViewSet.

--- core/models.py
+++ core/models.py
@@ -8,10 +8,14 @@ class Task(models.Model):
     ]
 
     title = models.CharField(max_length=50)
-    description = models.CharField(blank=True)
+    description = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
     due_date = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
     
+    class Meta:
+        ordering = ['-created_at']
