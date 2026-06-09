8285240992d837a36d9201dc26ae4f29776353da
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-09 10:55:20 +0530

    feat: add category field and add 30 tasks.

--- core/models.py
+++ core/models.py
@@ -10,6 +10,7 @@ class Task(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='todo')
+    category = models.CharField(max_length=20)
     due_date = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
