b2d8b9ba4a9b6646684a035078683247658ef1c0
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-15 13:20:46 +0530

    feat: added pagnition and search features.

--- core/views.py
+++ core/views.py
@@ -2,13 +2,22 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Task
 from .serializers import TaskSerializer, TaskFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer 
     lookup_field = 'pk'
-    filter_backends = [DjangoFilterBackend]
+    search_fields = ['title', 'description']
+    filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = TaskFilter
+    pagination_class = StandardPagination
     
     def get_serializer(self, *args, **kwargs):
         
