4efe26216144386e84702885f2266a0060f9070d
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-16 16:51:44 +0530

    feat: fixed settings file for frotend

--- taskmanager_api/settings.py
+++ taskmanager_api/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'taskmanager_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'core' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
