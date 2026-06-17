56f83702d228f81b179510cb450ee279ff9f9826
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-17 10:43:34 +0530

    .

--- taskmanager_api/urls.py
+++ taskmanager_api/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls, ),
    # path('api/tasks/', TemplateView.as_view(template_name='index.html')),
     path('api/', include('core.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    path('api/tasks/', TemplateView.as_view(template_name='index.html')),
 ]
