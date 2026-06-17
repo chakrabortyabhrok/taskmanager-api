29aa9f20d70ef4d707eab64aab1454383d94b3c2
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-17 09:14:35 +0530

    Fixed api/tasks routing

--- taskmanager_api/urls.py
+++ taskmanager_api/urls.py
@@ -20,7 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls, ),
+    path('api/tasks/', TemplateView.as_view(template_name='index.html')),
     path('api/', include('core.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('api/tasks/', TemplateView.as_view(template_name='index.html'))
 ]
