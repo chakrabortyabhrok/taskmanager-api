a58f9791443a4c6bec9efd971d13cf577b7a1161
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-06-20 11:14:32 +0530

    fixed filter bugs

--- core/filters.py
+++ core/filters.py
@@ -5,7 +5,7 @@ class TaskFilter(django_filters.FilterSet):
     """ Custom filter for Task model supporting status and category name filtering. """
     category = django_filters.CharFilter(
         field_name='category__name',
-        lookup_expr='iexact'
+        lookup_expr='icontains'
     )
     class Meta:
         model = Task
