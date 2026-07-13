7bf2d38ffed8006b80c68ac247d3450c2ae56c1e
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-07-13 13:33:55 +0530

    Debugging

--- taskmanager_api/settings.py
+++ taskmanager_api/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-change-this-in-production')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+#os.environ.get('DEBUG', 'False') == 
+DEBUG = 'True'
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
