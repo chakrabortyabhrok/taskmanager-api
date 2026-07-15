3eed5c1eb754068e472d11110f411c5fb259b5f2
Abhrok Chakraborty <chakrabortyabhrok@gmail.com>
2026-07-15 14:09:22 +0530

    fixing bug since 5 hours.

--- taskmanager_api/settings.py
+++ taskmanager_api/settings.py
@@ -95,10 +95,7 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
-            default=DATABASE_URL,
-            conn_max_age=600,           # Keep connections alive longer
-            conn_health_checks=True,    # Check connection health before using
-            ssl_require=True,
+            default=os.environ.get('DATABASE_URL')
         )
     }
     
